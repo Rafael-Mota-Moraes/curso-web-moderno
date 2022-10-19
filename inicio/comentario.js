c19d13e66e4ca274262b765df4e2e4365f4046df
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-19 18:01:26 -0300

    comentário de código

---
+++ inicio/comentario.js
@@ -0,0 +1,13 @@
+// Aqui temos o 1° passo do progroma
+console.log("Passo 1");
+// Aqui temos o 2° passo do progroma
+console.log("Passo 2");
+// Aqui temos o 3° passo do progroma
+console.log("Passo 3");
+
+/*
+  comentário
+  de
+  várias
+  linhas
+*/
