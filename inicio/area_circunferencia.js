3dd2153061e4e51443ecde2a1739a2d63e908335
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-20 18:10:40 -0300

    desafios

---
+++ inicio/area_circunferencia.js
@@ -0,0 +1,6 @@
+// PI  * r^2
+let raio = 10;
+let pi = Math.PI;
+
+let area = pi * raio ** 2;
+console.log(area);
---
+++ inicio/troca_valores.js
@@ -0,0 +1,9 @@
+let a = 7; // 94
+let b = 94; // 7
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log(a, b);
