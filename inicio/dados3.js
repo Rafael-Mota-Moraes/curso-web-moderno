a63adb931ffab75186295dd662856ab63e17df4c
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-20 18:02:38 -0300

    Trabalhando com dados

---
+++ inicio/dados3.js
@@ -0,0 +1,9 @@
+let idade = 31; // Number
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4568.32;
+console.log(typeof salario);
+
+let estaChovendo = false; // ou true
+console.log(estaChovendo);
