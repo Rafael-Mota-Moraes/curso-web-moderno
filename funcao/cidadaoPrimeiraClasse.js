c8186ba21e3e7098a0265f9b1e391b12ad993e83
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-25 11:32:10 -0300

    Cidadão de primeira classe

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,51 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
+
+// Criar de forma literal
+
+function fun1() {}
+
+// Armazenar em uma variável
+
+const fun2 = function () {};
+
+// Armazenar em um array
+
+const array = [
+  function (a, b) {
+    return a + b;
+  },
+  fun1,
+  fun2
+];
+
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de objeto
+
+const obj = {};
+obj.falar = function () {
+  return "opa";
+};
+
+console.log(obj.falar());
+
+// Passar função como param
+
+function run(fun) {
+  fun();
+}
+
+run(function () {
+  console.log("Executando");
+});
+
+// uma função pode retornar/conter uma função
+
+function soma(a, b) {
+  return function (c) {
+    return a + b + c;
+  };
+}
+
+console.log(soma(1, 2)(3));
