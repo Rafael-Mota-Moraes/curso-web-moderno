cd359d845679ca7b2ddc6b2d5d1a664a993de99b
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-11-12 12:27:25 -0300

    css com  webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,29 @@
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 4. referenciando plugin de externalização
 
 module.exports = {
-  mode: "development",
+  mode: "development", // 1. modo de desenvolvimento / outro modo é de produção (production) que é o padrão
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
-    path: __dirname + "/public"
+    path: __dirname + "/public" // 2. pasta de saída
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // 4. referenciando plugin de externalização
+      filename: "estilo.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, // 4. este plugin é conflitante com o style-loader, por isso ele ta comentado
+          //'style-loader', // 3. Adiciona CSS a DOM injetando a tag <style>
+          "css-loader" // 3. interpreta @import, url()...
+        ]
+      }
+    ]
   }
 };
