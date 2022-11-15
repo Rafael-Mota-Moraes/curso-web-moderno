e672aa6a8e550f9f675674ebdb4b23dbca780c2e
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-11-15 14:03:33 -0300

    Componentes Pai & Filho

--- modulo-react/src/index.js
+++ modulo-react/src/index.js
@@ -5,11 +5,17 @@ import Primeiro from "./componentes/Primeiro";
 import BomDia from "./componentes/BomDia";
 import { BoaNoite, BoaTarde } from "./componentes/Multiplos";
 import Saudacao from "./componentes/Saudacao";
+import Pai from "./componentes/Pai";
+import Filho from "./componentes/Filho";
 
 // const elemento = <h1>Olá Mundo</h1>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <Saudacao tipo="Bom Dia" nome="João" />
+    <Pai nome="João" sobrenome="Silva">
+      <Filho nome="Pedro" sobrenome="Silva" />
+      <Filho nome="Marcelo" sobrenome="Silva" />
+      <Filho nome="Maria" sobrenome="Silva" />
+    </Pai>
   </div>
 );
