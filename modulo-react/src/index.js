02ac68e59f00c34575344a4db6331179300fba1f
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-11-15 13:52:24 -0300

    Componentes de classe

--- modulo-react/src/index.js
+++ modulo-react/src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom/client";
 import Primeiro from "./componentes/Primeiro";
 import BomDia from "./componentes/BomDia";
 import { BoaNoite, BoaTarde } from "./componentes/Multiplos";
+import Saudacao from "./componentes/Saudacao";
 
 // const elemento = <h1>Olá Mundo</h1>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <BoaTarde nome="joão" />
-    <BoaNoite nome="Daniel" />
+    <Saudacao tipo="Bom Dia" nome="João" />
   </div>
 );
